b22fd3e60002fdcbd3a238b40ea4f73ef1f03d15
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-10-17 18:16:58 +0300

    app components created

---
+++ to-do-list/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Footer from './components/footer/footer';
+import List from './components/list';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      
+      <List />
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
