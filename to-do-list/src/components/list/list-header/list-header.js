63c0fdf7e6a63ddc60ead07f9021ba45d7dbcf5a
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-10-17 18:39:07 +0300

    create input and to add notes to list.
    - create input state.
    - add onChange and onSubmit method to input.

---
+++ to-do-list/src/components/list/list-header/list-header.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+// import './list-header.css'
+
+function ListHeader({ setListItem, listItems }) {
+
+	const initialInputValue = ""
+	const [input, setInput] = useState(initialInputValue);
+
+	const onChangeInput = (e) => {
+		setInput( e.target.value );
+	}
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		if (input === "") return false;
+
+		setListItem([...listItems, input]);
+		setInput(initialInputValue);
+	}
+
+	return (
+		<header className="header">
+			<h1>todos</h1>
+			<form onSubmit={onSubmit}>
+				<input
+					value={input}
+					onChange={onChangeInput}
+					className="new-todo"
+					placeholder="What needs to be done?"
+					autoFocus
+				/>
+			</form>
+		</header>
+	)
+}
+
+export default ListHeader;
