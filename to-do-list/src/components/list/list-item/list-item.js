ff31c056e318039349b8d942586341df5752d35d
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-10-19 12:19:00 +0300

    add click method to list item
    - check/uncheck the box
    - strike-through the completed list items
    - add bootstrap

--- to-do-list/src/components/list/list-item/list-item.js
+++ to-do-list/src/components/list/list-item/list-item.js
@@ -1,4 +1,3 @@
-
 import './list-item.css'
 
 function ListItem({ deleteListItem, listItems }) {
@@ -9,18 +8,27 @@ function ListItem({ deleteListItem, listItems }) {
 		deleteListItem(updatedListItems);
 	}
 
+
+	const onCheckboxClick = (i, e) => {
+		document.getElementById(i).checked = !document.getElementById(i).checked;
+
+		e.target.style.textDecoration
+			? e.target.style.removeProperty('text-decoration')
+			: e.target.style.setProperty('text-decoration', 'line-through');
+	}
+
 	return (
 		<section className="main" >
-			<label>
+			<button type="button" className="btn btn-warning">
 				Mark all as complete
-			</label>
+			</button>
 			<ul className="todo-list">
 				{listItems.map((listItem, i) => (
 					<li key={i} >
 						<div className="view">
 							<input id={i} className="listItemCheckbox" type="checkbox" />
-							<label className='listItemLabel' onClick={ (e) => onCheckboxClick(i,e) }>
-								{listItem} 
+							<label className='listItemLabel' onClick={(e) => onCheckboxClick(i, e)}>
+								{listItem}
 							</label>
 							<button id={i} onClick={onDelete} className="destroy"></button>
 						</div>
