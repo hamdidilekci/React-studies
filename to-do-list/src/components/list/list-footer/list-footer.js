7793d556169f979c107988e5e5bb1ae0af713d85
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-10-25 16:48:17 +0300

    deleteAllCompleted method added.
    -buttons added to list footer.
    - ** - showActive, showAll and showCompleted will be changed later - ** -

--- to-do-list/src/components/list/list-footer/list-footer.js
+++ to-do-list/src/components/list/list-footer/list-footer.js
@@ -1,26 +1,58 @@
 import React from 'react'
 
-function ListFooter({ listItems }) {
+function ListFooter({ setListItem, listItems }) {
+
+	const showActive = () => {
+		// document.querySelectorAll('.view')
+		// 	.forEach(el => {
+		// 		if (el.className.includes('active')) {
+		// 		el.classList.remove('invisible')
+		// 		} else {
+		// 			document.querySelectorAll('.completed').forEach(el => el.classList.add('invisible'));
+		// 		}
+		// 	});
+	}
+
+	const showAll = () => {
+		// document.querySelectorAll('.invisible').forEach(el => el.classList.remove('invisible'));
+	}
+
+	const showCompleted = () => {
+		// document.querySelectorAll('.view')
+		// 	.forEach(el => {
+		// 		if (el.className.includes('active')) {
+		// 			el.classList.add('invisible')
+		// 		} else if (el.className.includes ('completed')) {
+		// 			el.classList.remove('invisible')
+		// 		}
+		// 	});
+	}
+
+	const onDeleteAllCompleted = (e) => {
+
+		setListItem(prevState => prevState.filter(el => !el.isCompleted));
+	}
+
 	return (
 		<footer className="footer">
 			<span className="todo-count">
-				<strong>{listItems.length} </strong>
+				<strong> {listItems.length} </strong>
 				items left
 			</span>
 
 			<ul className="filters">
 				<li>
-					<a href={"#/"} className="selected">All</a>
+					<button className='btn btn-primary' onClick={showAll}>All</button>
 				</li>
 				<li>
-					<a href={"#/"}>Active</a>
+					<button className='btn btn-success' onClick={showActive}>Active</button>
 				</li>
 				<li>
-					<a href={"#/"}>Completed</a>
+					<button className='btn btn-dark' onClick={showCompleted}>Completed</button>
 				</li>
 			</ul>
 
-			<button className="clear-completed">
+			<button onClick={onDeleteAllCompleted} className="clear-completed">
 				Clear completed
 			</button>
 		</footer>
