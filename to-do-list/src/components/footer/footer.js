17e4b804df7ba3e0782215499a5fd2a221a8af1f
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-10-26 14:15:19 +0300

    edit footer text

--- to-do-list/src/components/footer/footer.js
+++ to-do-list/src/components/footer/footer.js
@@ -4,8 +4,8 @@ function Footer() {
   return (
     <footer className='info'>
       <p>Click to edit a todo</p>
-      <p>Created by <a href="https://d12n.me/">Dmitry Sharabin</a></p>
-	    <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+      <p>Created by <a href="#/">Hamdi DİLEKÇİ</a></p>
+	    <p>Part of <a href="#/">DİLEKÇİ A.Ş </a></p>
     </footer>
   )
 }
