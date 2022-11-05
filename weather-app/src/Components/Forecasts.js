14a914bd5f7ac865c98024f331a619f79e103c8e
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-11-05 18:23:27 +0300

    remove div tag

--- weather-app/src/Components/Forecasts.js
+++ weather-app/src/Components/Forecasts.js
@@ -5,22 +5,20 @@ import ForecastItem from './Forecast-item';
 
 function Forecasts() {
     const weather = useWeather();
-    
+
     if (weather.state !== 'loading...') {
         return (
-            <div className='container'>
-                <div className="elements" >
-                    {weather.time.map((t, index) => {
-                        return (
-                            <ForecastItem key={index}
-                                time={weather.time[index]}
-                                weatherCode={weather.weathercode[index]}
-                                temperature2mMax={weather.temperature_2m_max[index]}
-                                temperature2mMin={weather.temperature_2m_min[index]}
-                            />
-                        )
-                    })}
-                </div>
+            <div className="elements" >
+                {weather.time.map((t, index) => {
+                    return (
+                        <ForecastItem key={index}
+                            time={weather.time[index]}
+                            weatherCode={weather.weathercode[index]}
+                            temperature2mMax={weather.temperature_2m_max[index]}
+                            temperature2mMin={weather.temperature_2m_min[index]}
+                        />
+                    )
+                })}
             </div>
         )
     }
