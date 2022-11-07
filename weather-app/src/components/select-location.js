d3a4478785523e8261bb24f2ba087015295bfc62
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-11-07 16:25:03 +0300

    add enter button for update location
    - add gps button for get current location

--- weather-app/src/components/select-location.js
+++ weather-app/src/components/select-location.js
@@ -1,20 +1,24 @@
-import { useState } from 'react'
+// Hooks
 import useGeolocation from "react-hook-geolocation";
 
-function Input() {
+import {useWeather} from '../context/weatherContext'
+
+function SelectLocation() {
+    const { setLocation, currentLocation, setCurrentLocation } = useWeather();
+
     const geolocation = useGeolocation();
-
-    const [location, setLocation] = useState({
-        latitude: 11.11,
-        longitude: 22.22
-    })
-
-    const getLocation = (e) => {
-        e.preventDefault();
-        setLocation({
+    const enterLocation = () => {
+        setLocation( {
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude
+        } )
+    }
+    
+    const updateInput = () => {
+        setCurrentLocation( {
             latitude: geolocation.latitude,
             longitude: geolocation.longitude
-        })
+        } )
     }
 
     return !geolocation.error ? (
@@ -23,10 +27,10 @@ function Input() {
                 <div className="input-group">
                     <div className='mb-2'>
                         <div className="input-group">
-                            <input type="text" placeholder="latitude" className="form-control" value={location.latitude.toFixed(2)}/>
-                            <input type="text" placeholder="longitude" className="form-control" value={location.longitude.toFixed(2)}/>
-                            <button onClick={getLocation} className="input-group-text">O</button>
-                            <button type="submit" className="input-group-text">Enter</button>
+                            <input type="text" placeholder="latitude" className="form-control" value={currentLocation?.latitude.toFixed(2)}/>
+                            <input type="text" placeholder="longitude" className="form-control" value={currentLocation?.longitude.toFixed(2)}/>
+                            <button onClick={updateInput} className="input-group-text">O</button>
+                            <button onClick={enterLocation} type="submit" className="input-group-text">Enter</button>
                         </div>
                     </div>  
                 </div>
@@ -37,4 +41,4 @@ function Input() {
     );
 }
 
-export default Input;
+export default SelectLocation;
--- weather-app/src/context/weatherContext.js
+++ weather-app/src/context/weatherContext.js
@@ -1,23 +1,23 @@
 import { createContext, useState, useEffect, useContext } from "react";
 import axios from 'axios';
 
-
 const WeatherContext = createContext();
 
 export const WeatherProvider = ({children}) => {
-    
+    const [location, setLocation] = useState({latitude : 40.99, longitude: 20.99});
+    const [currentLocation, setCurrentLocation] = useState({latitude : 40.99, longitude: 20.99});
     const [weather, setWeather] = useState({state : 'loading...'});
 
     useEffect(() => {
         async function getWeather() {
-            await axios.get('https://api.open-meteo.com/v1/forecast?latitude=40.99&longitude=28.89&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto')
+            await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${location?.latitude}&longitude=${location?.longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto`)
             .then(res => setWeather(res.data.daily)) 
         }
         getWeather()
-    },[]);
+    },[location?.latitude, location?.longitude]);
 
     return (
-        <WeatherContext.Provider value={weather}>{children}</WeatherContext.Provider>
+        <WeatherContext.Provider value={ { weather, setLocation, currentLocation, setCurrentLocation } }>{children}</WeatherContext.Provider>
     );
 }
 
