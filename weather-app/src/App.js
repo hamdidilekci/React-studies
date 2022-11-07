3ec43aa76531c5435ca82ecb2e1c18339088ca26
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-11-07 16:23:56 +0300

    set location when enter button clicked

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 
 import Forecasts from './components/forecasts';
-import Input from './components/select-location';
+import SelectLocation from './components/select-location';
 import {WeatherProvider} from './context/weatherContext';
 
 
 function App() {
   return (
     <div className="App">
-      <WeatherProvider>
-        <div className='container'>
-          <Input/>
-          <Forecasts/>
-        </div>
-      </WeatherProvider>
+        <WeatherProvider>
+          <div className='container'>
+            <SelectLocation/>
+            <Forecasts/>
+          </div>
+        </WeatherProvider>
     </div>
   );
 }
