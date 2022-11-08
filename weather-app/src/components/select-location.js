bc1eb793cc616f692b05f14efcbfe2f4a634e4ea
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-11-08 23:05:55 +0300

    set state when input change

--- weather-app/src/components/select-location.js
+++ weather-app/src/components/select-location.js
@@ -1,38 +1,64 @@
+import { useState } from 'react';
 // Hooks
 import useGeolocation from 'react-hook-geolocation';
-
+// context
 import { useWeather } from '../context/weatherContext';
-
-import { useState } from 'react';
+// icon
+import LocationMark from '../icons/location-mark';
 
 function SelectLocation() {
     const { location, setLocation } = useWeather();
     const [currentLocation, setCurrentLocation] = useState({ latitude: location.latitude, longitude: location.longitude });
 
     const geolocation = useGeolocation();
-    const enterLocation = () => {
-        setLocation({
-            latitude: currentLocation.latitude,
-            longitude: currentLocation.longitude
-        });
-    };
-
+    
     const updateInput = () => {
         setCurrentLocation({
             latitude: geolocation.latitude,
             longitude: geolocation.longitude
         });
     };
+    
+    const handleInputChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
 
+        let newLocation = {...currentLocation}
+        newLocation[name] = +value
+        
+        setCurrentLocation(newLocation);
+    };
+    
+    const enterLocation = () => {
+        setLocation({
+            latitude: currentLocation?.latitude,
+            longitude: currentLocation?.longitude
+        });
+    };
+    
     return !geolocation.error ? (
         <div>
             {
                 <div className="input-group">
                     <div className='mb-2'>
                         <div className="input-group">
-                            <input type="text" placeholder="latitude" className="form-control" value={currentLocation?.latitude.toFixed(2)}/>
-                            <input type="text" placeholder="longitude" className="form-control" value={currentLocation?.longitude.toFixed(2)}/>
-                            <button onClick={updateInput} className="input-group-text">O</button>
+                            <input 
+                                value={currentLocation?.latitude?.toFixed(2)}
+                                onChange={ handleInputChange } 
+                                name='latitude'
+                                type="text" 
+                                placeholder="latitude" 
+                                className="form-control" 
+                            />
+                            <input 
+                                value={currentLocation?.longitude?.toFixed(2)}
+                                onChange={ handleInputChange } 
+                                name='longitude'
+                                type="text" 
+                                placeholder="longitude" 
+                                className="form-control" 
+                            />
+                            <button onClick={updateInput} className="input-group-text"><LocationMark/></button>
                             <button onClick={enterLocation} type="submit" className="input-group-text">Enter</button>
                         </div>
                     </div>
