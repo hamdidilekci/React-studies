af51273667a8bcc412316b1dd46108ebfc82710c
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-11-08 21:54:08 +0300

    add location mark

---
+++ weather-app/src/icons/location-mark.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function LocationMark() {
+    return (
+        <svg
+            style={{ color: 'rgb(0, 0, 0)' }}
+            width="20"
+            height="24"
+            fill="currentColor"
+            className="bi bi-geo-alt-fill"
+            viewBox="0 0 16 16">
+            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" fill="#000000">
+            </path>
+        </svg>
+    )
+}
+
+
