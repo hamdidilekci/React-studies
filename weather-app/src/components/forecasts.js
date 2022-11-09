6bf70c5d6616197fd6d9c06b0c6c1fc77c4877c7
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-11-09 16:42:05 +0300

    add details and cuurentDay component

--- weather-app/src/components/forecasts.js
+++ weather-app/src/components/forecasts.js
@@ -1,7 +1,10 @@
 import '../App.css';
 import { useWeather } from '../context/weatherContext';
 
+import CurrentDay from './current-day';
+import Details from './details';
 import ForecastItem from './forecast-item';
+
 
 function Forecasts() {
     const { weather } = useWeather();
@@ -10,38 +13,56 @@ function Forecasts() {
         return (
             <div className='row'>
                 <div className='row weather-card'>
-                    <div className="col-md-3 today-card rounded-start d-inline-flex flex-column justify-content-center align-items-center" style={{ backgroundColor: 'green' }}>
-                        <div className='flex-fill me-auto mt-3'>
-                            <h1>Sunday</h1>
-                            <h3>June 15</h3>
-                        </div> 
-                        <div className='flex-end me-auto mb-3'>
-                            <h1>17C</h1>
-                            <h2>Heavy cloud</h2>
-                        </div>
+                    <div  className="col-md-3 rounded-start today-card ">
+                        {weather.time.map((t, index) => {
+                            if (index !== 0) {
+                                return <div key={index}></div>
+                            }
+                            return (
+                                <div key={index}>
+                                    <CurrentDay
+                                        time={weather.time[0]}
+                                        weatherCode={weather.weathercode[0]}
+                                        temperature2mMax={weather.temperature_2m_max[0]}
+                                        temperature2mMin={weather.temperature_2m_min[0]}
+                                    />
+                                </div>
+                            )
+                        })}
                     </div>
-                    <div className="col-md-9 rounded-end week-card pb-4 pt-3" style={{ backgroundColor: 'red' }}>
+                    <div className="col-md-9 rounded-end week-card pb-4 pt-3">
                         <div>
-                            <div>Max Temp:</div>
-                            <div>Min Temp:</div>
-                            <div>Sunrise:</div>
-                            <div>Sunset:</div>
-                            <div>Max Wind Speed:</div>
-                        </div>
-                        <hr />
-                        <div className="elements" >
-                            <div className="row me-5">
-                                {weather.time.map((t, index) => {
-                                    if (index === 0) {
-                                        return <div></div>
-                                    }
-                                    return ( 
-                                        <div className="col-md-2 p-5">
-                                            <ForecastItem key={index}
-                                            time={weather.time[index]}
-                                            weatherCode={weather.weathercode[index]}
+                            {weather.time.map((t, index) => {
+                                if (index !== 0) {
+                                    return <div key={index}></div>
+                                }
+                                return (
+                                    <div key={index}>
+                                        <Details
                                             temperature2mMax={weather.temperature_2m_max[index]}
                                             temperature2mMin={weather.temperature_2m_min[index]}
+                                            sunrise={weather.sunrise[index]}
+                                            sunset={weather.sunset[index]}
+                                            windspeed10mMax={weather.windspeed_10m_max[index]}
+                                        />
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <hr />
+                        <div className="elements rounded p-2" >
+                            <div className="row">
+                                {weather.time.map((t, index) => {
+                                    if (index === 0) {
+                                        return <div key={index}></div>
+                                    }
+                                    return (
+                                        <div key={index} className="col-md-2 d-flex flex-row justify-content-center">
+                                            <ForecastItem
+                                                time={weather.time[index]}
+                                                weatherCode={weather.weathercode[index]}
+                                                temperature2mMax={weather.temperature_2m_max[index]}
+                                                temperature2mMin={weather.temperature_2m_min[index]}
                                             />
                                         </div>
                                     )
