66bbc9594f6a3ee63efedee3452093310003406c
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-11-10 15:20:44 +0300

    get short and long day name

--- weather-app/src/components/forecast-item.js
+++ weather-app/src/components/forecast-item.js
@@ -2,27 +2,27 @@ import React from 'react'
 
 import '../App.css'
 
-import getSymbolByWeatherCode from '../helpers/get-symbol-by-weather-code'
+import getIconByWeatherCode from '../helpers/get-icon-by-weather-code'
 import getDayName from '../helpers/get-day-name'
 
-function ForecastItem({time, weatherCode, temperature2mMax, temperature2mMin}) {
+function ForecastItem({ time, weatherCode, temperature2mMax, temperature2mMin }) {
 
-    const [symbol] = getSymbolByWeatherCode(weatherCode);
+    const {icon} = getIconByWeatherCode(weatherCode);
 
-    const day = getDayName(time, "en-US");
+    const {shortDay} = getDayName(time, "en-US");
 
-    const averageTemp = (parseInt( temperature2mMax + temperature2mMax )/2).toFixed()
+    const averageTemp = (parseInt(temperature2mMax + temperature2mMin) / 2).toFixed()
 
     return (
         <div>
             <div className="card-img-top d-flex justify-content-center">
-                {symbol}
+                {icon}
             </div>
             <div className="card-body">
-                <span className='day-name'>{day}</span>
-                <span className="card-text d-flex justify-content-center fw-bold">
+                <h5 className='day-name'>{shortDay}</h5>
+                <h5 className="card-text d-flex justify-content-center fw-bold">
                     {`${averageTemp}°`}
-                </span>
+                </h5>
             </div>
         </div>
     )
