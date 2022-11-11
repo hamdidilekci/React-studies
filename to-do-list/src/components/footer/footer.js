a75a67e2672ffba1ce86034c4e119e8e90e93563
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-11-11 22:41:26 +0300

    add checkbox and make list items editible

--- to-do-list/src/components/footer/footer.js
+++ to-do-list/src/components/footer/footer.js
@@ -3,9 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className='info'>
-      <p>Click to edit a todo</p>
       <p>Created by <a href="#/">Hamdi DİLEKÇİ</a></p>
-	    <p>Part of <a href="#/">DİLEKÇİ A.Ş </a></p>
     </footer>
   )
 }
--- to-do-list/src/components/list/list-item/list-item.js
+++ to-do-list/src/components/list/list-item/list-item.js
@@ -1,11 +1,6 @@
 
 function ListItem({ setListItem, listItems }) {
-
-	const onDelete = (e) => {
-		 // eslint-disable-next-line eqeqeq
-		setListItem(prevState => prevState.filter((el,index)=> index != e.target.id));
-	}
-
+	
 	const onItemClick = (i, e, from) => {
 		setListItem(prevState => prevState.map((el, index) => {
 			if (index === i) {
@@ -15,17 +10,47 @@ function ListItem({ setListItem, listItems }) {
 		}));
 	}
 
+	const onChangeItems = (i, e) => {
+		setListItem(prevState => prevState.map((el, index) => {
+			if (index === i) {
+				return {...el, name:e.target.value}
+			}
+			return el;
+		}));
+	}
+	
+	const onDelete = (e) => {
+		// eslint-disable-next-line eqeqeq
+		setListItem(prevState => prevState.filter((el,index)=> index != e.target.id));
+	}
+	
 	return (
 		<section className="main" >
 			<ul className="todo-list">
 				{listItems.map((listItem, i) => (
-					<li key={i} >
-						{console.log(listItem.isVisible)}
-						<div className={(listItem.isCompleted ? 'view completed' : 'view') + ' ' + (listItem.isVisible ? '' : 'hidden')}>
-							<label onClick={(e) => onItemClick(i, e, 'label')}>
-								{listItem.name}
-							</label>
-							<button id={i} onClick={onDelete} className="destroy"></button>
+					<li key={i} className='d-flex justify-content-between'>
+						<div className="d-flex justify-content-center">
+							<input 
+								onClick={(e) => onItemClick(i, e, 'label')} 
+								type="checkbox"
+							/>
+						</div>
+						<div>
+							<input
+								onChange={(e) => onChangeItems(i, e)}
+								value={listItem.name} 
+								className={
+									(listItem.isCompleted ? 'view completed' : 'view')
+									+ ' ' + 
+									(listItem.isVisible ? '' : 'hidden')
+								}
+							/>
+						</div>	
+						<div>
+							<button 
+								id={i} onClick={onDelete} 
+								className="destroy m-0">
+							</button>
 						</div>
 					</li>
 				))}
