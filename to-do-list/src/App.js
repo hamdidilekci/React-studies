0cca9eaf59339f998890fbf67fe27b5037f4344b
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-11-12 02:26:36 +0300

    add/remove css and format document

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -2,15 +2,10 @@ import './App.css';
 import Footer from './components/footer/footer';
 import List from './components/list';
 
-
-
 function App() {
   return (
     <div className="App">
-      <header></header>
-      
       <List />
-      
       <Footer />
     </div>
   );
--- to-do-list/src/components/list/list.js
+++ to-do-list/src/components/list/list.js
@@ -1,8 +1,10 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import ListHeader from "./list-header/list-header"
-import ListItem from "./list-item/list-item"
-import ListFooter from "./list-footer/list-footer"
+import ListHeader from "./list-header/list-header";
+import ListItem from "./list-item/list-item";
+import ListFooter from "./list-footer/list-footer";
+
+import './list.css';
 
 function List() {
 
@@ -10,17 +12,17 @@ function List() {
 
     return (
         <div className="todoapp">
-        <header>
-            <ListHeader setListItem={setListItems} listItems={listItems}/>
-        </header>
-        <section>
-            <ListItem setListItem={setListItems} listItems={listItems} />
-        </section>
-        <footer>
-            <ListFooter setListItem={setListItems} listItems={listItems}/>
-        </footer>
-    </div>
-    )
-  }
+            <header>
+                <ListHeader setListItem={setListItems} listItems={listItems} />
+            </header>
+            <section>
+                <ListItem setListItem={setListItems} listItems={listItems} />
+            </section>
+            <footer>
+                <ListFooter setListItem={setListItems} listItems={listItems} />
+            </footer>
+        </div>
+    );
+};
 
 export default List;
