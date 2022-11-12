e0526bb4c040445a6cb49f55a91a51413abd46ae
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2022-11-12 15:37:31 +0300

    show border when edit input and dont show when leave input

--- to-do-list/src/components/list/list-item/list-item.js
+++ to-do-list/src/components/list/list-item/list-item.js
@@ -1,47 +1,53 @@
 import './list-item.css'
 
 function ListItem({ setListItem, listItems }) {
-	
+
 	const onCheckboxClick = (i) => {
 		setListItem(prevState => prevState.map((el, index) => {
 			if (index === i) {
-				return {...el, isCompleted:!el.isCompleted}
+				return { ...el, isCompleted: !el.isCompleted }
 			}
 			return el;
 		}));
 	}
 
-	const onInputClick = (i) => {
+	const focusFunc = (i) => {
 		setListItem(prevState => prevState.map((el, index) => {
 			if (index === i) {
-				return {...el, isEditing:!el.isEditing}
+				return { ...el, isEditing: true }
 			}
-			return el;
+			return { ...el, isEditing: false };
+		}));
+	}
+
+	const blurFunc = (i) => {
+		setListItem(prevState => prevState.map((el, index) => {
+			return { ...el, isEditing: false };
 		}));
 	}
 
 	const onChangeItems = (i, e) => {
 		setListItem(prevState => prevState.map((el, index) => {
 			if (index === i) {
-				return {...el, name:e.target.value}
+				return { ...el, name: e.target.value }
 			}
 			return el;
 		}));
 	}
-	
+
 	const onDelete = (e) => {
 		// eslint-disable-next-line eqeqeq
-		setListItem(prevState => prevState.filter((el,index)=> index != e.target.id));
+		setListItem(prevState => prevState.filter((el, index) => index != e.target.id));
 	}
-	
+
 	return (
 		<section className="main" >
 			<ul className="todo-list">
 				{listItems.map((listItem, i) => (
 					<li key={i} className='d-flex justify-content-between'>
 						<div>
-							<input 
-								onClick={(e) => onCheckboxClick(i)} 
+							<input
+								onClick={(e) => onCheckboxClick(i)}
 								type="checkbox"
 								checked={listItem.isCompleted === true}
 								hidden={listItem.isVisible === false}
@@ -51,17 +57,16 @@ function ListItem({ setListItem, listItems }) {
 						<div>
 							<input
 								onChange={(e) => onChangeItems(i, e)}
-								value={listItem.name} 
-								className={(listItem.isCompleted ? 'view completed' : 'view')}
+								value={listItem.name}
+								className={(listItem.isCompleted ? 'view completed' : 'view') + ' ' + (listItem.isEditing ? 'edit' : '')}
 								hidden={listItem.isVisible === false}
-								autoFocus
-								onClick={(e) => onInputClick(i)}
-								style={listItem.isEditing ? {borderWidth: '2px'} : {borderWidth: '0px'}}
+								onFocus={(e) => focusFunc(i, e)}
+								onBlur={(e) => blurFunc(i, e)}
 							/>
-						</div>	
+						</div>
 						<div>
-							<button 
-								id={i} onClick={onDelete} 
+							<button
+								id={i} onClick={onDelete}
 								className="destroy m-0">
 							</button>
 						</div>
